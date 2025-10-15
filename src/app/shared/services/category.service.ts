9fd61ad394d03632b8c1c26b174369d18814370d
georgyguei <georgy.guei@efrei.net>
2025-10-15 10:16:29 +0200

    Add CategoryService to manage category data retrieval

---
+++ src/app/shared/services/category.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Category {
+  id: number;
+  name: string;
+  description: string;
+  icon: string;
+  color: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Get all categories from the database
+   */
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${this.apiUrl}/categories`);
+  }
+
+  /**
+   * Get a specific category by ID
+   */
+  getCategoryById(id: number): Observable<Category> {
+    return this.http.get<Category>(`${this.apiUrl}/categories/${id}`);
+  }
+
+  /**
+   * Get questions for a specific category
+   */
+  getQuestionsByCategory(categoryId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/questions?categoryId=${categoryId}`);
+  }
+}
